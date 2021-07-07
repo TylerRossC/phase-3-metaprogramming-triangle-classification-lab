4aecce0732c80e1c1c1aa4c8f41994f8d02144b7
tyler <tylercharawell@gmail.com>
2021-07-07 12:17:54 -0700

    commit tree f00eadca9259cdf9497c3f75c12e2146e74d3060

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if ((a == b) && (a == c) && (b == c))
+      return :equilateral
+    elsif ((a == b) || (a == c) || (b == c))
+      return :isosceles
+    else
+      return :scalene
+    end
+    raise TriangleError if ((a == 0) && (b == 0) && (c == 0))
+  rescue TriangleError => error
+    puts error.message
+  end
+    
+    # if triangle.class != Triangle
+    #   begin
+    #     raise TriangleError
+    #   rescue TriangleError => error
+    #     puts error.message
+    #   end
+    # else
+    #   triangle = self
+    # end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "illegal triangle"
+    end
+  end
 end
